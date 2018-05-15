193af74ee9314e8fc075a70f9f2ac68b6ea0d827
Matt Zuzolo <mattzuzolo@outlook.com>
2018-05-15 03:03:45 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,24 @@
 class Dog  
   
-  
-  def initialize(breed, breed = "mutt")
-    @breed = breed
-  end
-  
-  def breed=(breed)
-    @breed = breed
-  end
-  
-  def breed
-    @breed
+  def initialize(name)
+    @name = name
   end  
+  
   
 
 end
+
+
+
+
+# def initialize(breed, breed = "mutt")
+#     @breed = breed
+#   end
+  
+#   def breed=(breed)
+#     @breed = breed
+#   end
+  
+#   def breed
+#     @breed
+#   end  
